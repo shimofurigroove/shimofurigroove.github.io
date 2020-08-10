e133c4c747accf041b55c9f259df76a4ac8c980b
shimofurigroove <shimofurigroove@gmail.com>
2020-08-10 20:42:36 +0900

    prerender-spa-plugin追加

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,29 @@
+const path = require('path')
+const PrerenderSPAPlugin = require('prerender-spa-plugin')
+
 module.exports = {
-    css: {
-      loaderOptions: {
-        scss: {
-          prependData: '@import "./src/assets/sass/prepends.scss";'
-        }
+  configureWebpack: () => {
+    return {
+      plugins: [
+        new PrerenderSPAPlugin({
+          staticDir: path.join(__dirname, 'docs'),
+          routes: [
+            '/',
+            '/2019-12-08',
+            '/2020-01-31',
+            '/2020-07-22'
+          ]
+        })
+      ]
+    }
+  },
+  css: {
+    loaderOptions: {
+      scss: {
+        prependData: '@import "./src/assets/sass/prepends.scss";'
       }
-    },
-    publicPath: './',
-    outputDir: 'docs'
-  };
+    }
+  },
+  publicPath: './',
+  outputDir: 'docs'
+};
