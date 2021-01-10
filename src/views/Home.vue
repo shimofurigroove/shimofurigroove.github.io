7604439d7e2c4d08ce2ed9b77fcf9720f795f842
shimofurigroove <shimofurigroove@gmail.com>
2021-01-10 12:36:31 +0900

    add event

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -26,6 +26,29 @@
 			</section>
 			<section id="events">
 				<h2>🦅開催予定 Upcoming Events</h2>
+				<div class="row">
+					<div class="col-md-6">
+						<p><img src="/images/10/flyer.png" /></p>
+					</div>
+					<div class="col-md-6">
+						<p>
+							あけましておめでとうございます！<br />
+							今年ものんびりDJの練習会のようなことを金熊さんで開催します。PC、コントローラーなど持参していただき自由にDJしてOKです。<br />
+							※ご連絡先をご記入いただいた後、Open DJにご参加ください。<br />
+							※Open DJは先着順です。時間や進行の都合上、DJできない場合もあります。ご了承ください。
+						</p>
+						<div class="embed-container">
+							<iframe
+								src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12591.061337750092!2d140.1117743!3d37.9125474!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x44f6ae37e307a919!2z6YeR54aK6aOf5aCC!5e0!3m2!1sja!2sjp!4v1576427433769!5m2!1sja!2sjp"
+								width="600"
+								height="450"
+								frameborder="0"
+								style="border:0;"
+								allowfullscreen=""
+							></iframe>
+						</div>
+					</div>
+				</div>
 				<ul class="date-list">
 					<li>
 						<p class="date">2021.01.27 wed</p>
